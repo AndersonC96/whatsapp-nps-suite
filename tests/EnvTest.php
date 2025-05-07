4d3df8c1a116ce3d1c4625e8e7bdd8c90ef52340
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-07 17:35:54 -0300

    Update EnvTest.php

--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -3,11 +3,11 @@
     use Core\Env;
     class EnvTest extends TestCase {
         public function testEnvLoadsVariable() {
-            // Simula carregamento de .env temporário
             $tempEnv = __DIR__ . '/.env.test';
-            file_put_contents($tempEnv, "TEST_VAR=123");
+            file_put_contents($tempEnv, "TEST_KEY=abc123");
             Env::load($tempEnv);
-            $this->assertEquals('123', getenv('TEST_VAR'));
-            unlink($tempEnv); // limpa o teste
+            $this->assertEquals('abc123', getenv('TEST_KEY'));
+            $this->assertEquals('abc123', $_ENV['TEST_KEY']);
+            unlink($tempEnv);
         }
     }
