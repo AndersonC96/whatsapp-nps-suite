ae00e6d101f8429a694461b7d2d207143be9fb87
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-08 11:43:44 -0300

    Update 20250508_create_responses_table.php

--- database/migrations/20250508_create_responses_table.php
+++ database/migrations/20250508_create_responses_table.php
@@ -2,7 +2,6 @@
     use Core\Database;
     return function () {
         $db = Database::connect();
-        $sql = "CREATE TABLE IF NOT EXISTS responses (id INT AUTO_INCREMENT PRIMARY KEY, campaign_id INT, client_id INT, nota INT CHECK (nota BETWEEN 0 AND 10), comentario TEXT,recebido_em DATETIME, FOREIGN KEY (campaign_id) REFERENCES campaigns(id), FOREIGN KEY (client_id) REFERENCES clients(id))
-    \";
-    $db->exec($sql);
-};
+        $sql = "CREATE TABLE IF NOT EXISTS responses (id INT AUTO_INCREMENT PRIMARY KEY, campaign_id INT, client_id INT, nota INT CHECK (nota BETWEEN 0 AND 10), comentario TEXT,recebido_em DATETIME, FOREIGN KEY (campaign_id) REFERENCES campaigns(id), FOREIGN KEY (client_id) REFERENCES clients(id))";
+        $db->exec($sql);
+    };
