1139a103e3c1383aa0622b66200906374de1fe0f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-08 12:25:59 -0300

    Create header.php

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title><?= $title ?? 'NPS Platform' ?></title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+    </head>
+    <body>
+        <section class="section">
+            <div class="container">
