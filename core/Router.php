c4fc926254dd24c1ac893f118e0a532645dff477
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-08 16:14:40 -0300

    Update Router.php

--- core/Router.php
+++ core/Router.php
@@ -2,10 +2,8 @@
     namespace Core;
     class Router {
         public function dispatch() {
-            // Captura a URI corretamente, mesmo em subpastas como /whatsapp-nps-suite/public
-            $scriptName = dirname($_SERVER['SCRIPT_NAME']);
-            $uri = str_replace($scriptName, '', $_SERVER['REQUEST_URI']);
-            $uri = parse_url($uri, PHP_URL_PATH);
+            // Captura a URI sem considerar diretórios
+            $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
             $uri = trim($uri, '/');
             $segments = explode('/', $uri);
             $controllerName = !empty($segments[0]) ? ucfirst($segments[0]) . 'Controller' : 'HomeController';
